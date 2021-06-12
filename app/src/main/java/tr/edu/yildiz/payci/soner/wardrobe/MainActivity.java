80261b749f168d77b528b5a5889d93c3a9a49a28
Soner Paycı <soner.payci@turna.com>
2021-06-12 18:08:16 +0300

    Initial App

---
+++ app/src/main/java/tr/edu/yildiz/payci/soner/wardrobe/MainActivity.java
@@ -0,0 +1,14 @@
+package tr.edu.yildiz.payci.soner.wardrobe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
